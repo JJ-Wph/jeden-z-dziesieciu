30aa03e1d3e52fdb1103260e307051cdd8e90f06
JJ-Wph <kuba.wypych92@gmail.com>
2023-06-09 15:01:18 +0200

    all audio files added, day before release

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,101 @@
+<template>
+
+  <!-- Menu Główne -->
+  <div v-if="!player&&!playerFinal&&!host&&!hostComplete">
+    <button @click="toggleFullScreen">Pełny ekran</button>
+    <h1>Kim jesteś?</h1>
+    <button @click="player=!player">Graczem</button>
+    <button @click="playerFinal=!playerFinal">Graczem w finale</button>
+    <button @click="host=!host">Prowadzącym</button>
+  </div>
+
+  <!-- Jesteś Graczem -->
+  <div v-if="player&&!playerDataCompleted">
+    <h2>Podaj swoje imię i numer stanowiska</h2>
+    <div class="inputholder">
+      <input v-model="playerName" type="text" placeholder="imię">
+      <input v-model="playerNumber" type="number">
+      <button @click="playerDataCompleted=!playerDataCompleted">Gotowe</button>
+    </div>
+  </div>
+  <!-- Panel Gracza -->
+  <div class="playerholder" v-if="playerDataCompleted">
+    <div class="chanceholder">
+      <div class="chance" :class="{ green: isOn1 }" @touchstart="isOn1=!isOn1"></div>
+      <div class="chance" :class="{ green: isOn2 }" @touchstart="isOn2=!isOn2"></div>
+      <div class="chance" :class="{ green: isOn3 }" @touchstart="isOn3=!isOn3"></div>
+    </div>
+    <h1>{{playerName}}</h1>
+    <h1>{{playerNumber}}</h1>
+  </div>
+
+  <!-- Jesteś Graczen Finałowym -->
+  <div v-if="playerFinal&&!playerFinalDataCompleted">
+    <h2>Podaj swoje imię i liczbę zachowanych szans z poprzedniego etapu</h2>
+    <div class="inputholder">
+      <input v-model="playerName" type="text" placeholder="imię">
+      <input v-model="playerPoints" type="number">
+      <button @click="playerFinalDataCompleted=!playerFinalDataCompleted">Gotowe</button>
+    </div>
+  </div>
+
+  <!-- Panel Gracza Finałowego -->
+  <div class="playerholder" v-if="playerFinalDataCompleted">
+    <div class="chanceholder">
+      <div class="chance" :class="{ green: isOn1 }" @touchstart="isOn1=!isOn1"></div>
+      <div class="chance" :class="{ green: isOn2 }" @touchstart="isOn2=!isOn2"></div>
+      <div class="chance" :class="{ green: isOn3 }" @touchstart="isOn3=!isOn3"></div>
+    </div>
+    <div class="pointsholder">
+      <div class="pointsdiv" @touchstart="playerPoints-=10">
+      </div>
+      <div class="pointsdiv">
+        <h1>{{playerName}}</h1>
+        <h1>{{playerPoints}}</h1>
+      </div>
+      <div class="pointsdiv" @touchstart="playerPoints+=10">
+      </div>
+    </div>
+  </div>
+
+  <!-- Jesteś Prowadzącym -->
+  <div v-if="host">
+    <h1>Jak jesteś taki cwany to podaj hasło:</h1>
+    <div class="inputholder">
+      <input type="text" v-model="password" name="" id="">
+      <h2 v-if="idiot">Guwniarzu zasrany zajebany</h2>
+      <button @click="activateHost">Ok</button>
+    </div>
+  </div>
+
+  <!-- Panel Prowadzącego -->
+  <div v-if="hostComplete">
+    <div class="buttonholder">
+      <button style="width: 100px; height: 100px" @click="dobrze.play()">DOBRZE</button>
+      <button style="width: 100px; height: 100px" @click="zle.play()">ŹLE</button>
+    </div>
+    <div class="buttonholder">
+      <button style="width: 80px; height: 80px" @click="przerywnik.play()">Brake</button>
+      <button style="width: 80px; height: 80px" @click="intro1.play()">Intro1</button>
+      <button style="width: 80px; height: 80px" @click="intro2.play()">Intro2</button>
+      <button style="width: 80px; height: 80px" @click="intro2.pause()">Intro2 stop</button>
+      <button style="width: 80px; height: 80px" @click="adriatica.play()">Zegarki</button>
+    </div>
+
+    <button @click="audio1.play()">MUZYCZNE 1</button>
+    <button @click="audio2.play()">MUZYCZNE 2</button>
+    <button @click="audio3.play()">MUZYCZNE 3</button>
+    <button @click="audio4.play()">MUZYCZNE 4</button>
+    <button @click="audio5.play()">MUZYCZNE 5</button>
+    <button @click="audio6.play()">MUZYCZNE 6</button>
+    
+
+
+  </div>
+
+
+</template>
+
 <script setup>
   import {ref} from 'vue'
 
@@ -20,6 +118,24 @@
   const isOn2 = ref(true);
   const isOn3 = ref(true);
 
+  const dobrze = new Audio('./src/assets/dobrze.mp3');
+  const zle = new Audio('./src/assets/zle.mp3');
+  const przerywnik = new Audio('./src/assets/przerywnik.mp3');
+  const adriatica = new Audio('./src/assets/adriatica.mp3')
+  const intro1 = new Audio('./src/assets/intro1.mp3');
+  const intro2 = new Audio('./src/assets/intro2.mp3');
+
+  intro2.playbackRate = 1.25;
+
+  const audio1 = new Audio('./src/assets/pytanie1.mp3');
+  const audio2 = new Audio('./src/assets/pytanie2.mp3');
+  const audio3 = new Audio('./src/assets/pytanie3.mp3');
+  const audio4 = new Audio('./src/assets/pytanie4.mp3');
+  const audio5 = new Audio('./src/assets/pytanie5.mp3');
+  const audio6 = new Audio('./src/assets/pytanie6.mp3');
+
+  document.documentElement.requestFullscreen();
+
   function activateHost() {
     if(password.value === 'wdupetrzaslo') {
       host.value = false;
@@ -30,102 +146,30 @@
     }
   }
 
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+  }
+
 </script>
-
-<template>
-
-  <!-- Menu Główne -->
-  <div v-if="!player&&!playerFinal&&!host&&!hostComplete">
-    <h1>Kim jesteś?</h1>
-    <button @click="player=!player">Graczem</button>
-    <button @click="playerFinal=!playerFinal">Graczem w finale</button>
-    <button @click="host=!host">Prowadzącym</button>
-  </div>
-
-  <!-- Jesteś Graczem -->
-  <div v-if="player&&!playerDataCompleted">
-    <h1>Podaj swoje imię i numer stanowiska</h1>
-    <div class="inputholder">
-      <input v-model="playerName" type="text" placeholder="imię">
-      <input v-model="playerNumber" type="number">
-      <button @click="playerDataCompleted=!playerDataCompleted">Gotowe</button>
-    </div>
-  </div>
-  <!-- Panel Gracza -->
-  <div class="playerholder" v-if="playerDataCompleted">
-    <div class="chanceholder">
-      <div class="chance" :class="{ green: isOn1 }" @touchstart="isOn1=!isOn1"></div>
-      <div class="chance" :class="{ green: isOn2 }" @touchstart="isOn2=!isOn2"></div>
-      <div class="chance" :class="{ green: isOn3 }" @touchstart="isOn3=!isOn3"></div>
-    </div>
-    <h1>{{playerName}}</h1>
-    <h1>{{playerNumber}}</h1>
-  </div>
-
-  <!-- Jesteś Graczen Finałowym -->
-  <div v-if="playerFinal&&!playerFinalDataCompleted">
-    <h1>Podaj swoje imię i liczbę zachowanych szans z poprzedniego etapu</h1>
-    <div class="inputholder">
-      <input v-model="playerName" type="text" placeholder="imię">
-      <input v-model="playerPoints" type="number">
-      <button @click="playerFinalDataCompleted=!playerFinalDataCompleted">Gotowe</button>
-    </div>
-  </div>
-
-  <!-- Panel Gracza Finałowego -->
-  <div class="playerholder" v-if="playerFinalDataCompleted">
-    <div class="chanceholder">
-      <div class="chance" :class="{ green: isOn1 }" @touchstart="isOn1=!isOn1"></div>
-      <div class="chance" :class="{ green: isOn2 }" @touchstart="isOn2=!isOn2"></div>
-      <div class="chance" :class="{ green: isOn3 }" @touchstart="isOn3=!isOn3"></div>
-    </div>
-    <h1>{{playerName}}</h1>
-    <h1>{{playerPoints}}</h1>
-  </div>
-
-  <!-- Jesteś Prowadzącym -->
-  <div v-if="host">
-    <h1>Jak jesteś taki cwany to podaj hasło:</h1>
-    <div class="inputholder">
-      <input type="text" v-model="password" name="" id="">
-      <h2 v-if="idiot">Guwniarzu zasrany zajebany</h2>
-      <button @click="activateHost">Ok</button>
-    </div>
-  </div>
-
-  <!-- Panel Prowadzącego -->
-  <div v-if="hostComplete">
-    <div class="inputholder">
-      <iframe width="110" height="110" src="https://www.myinstants.com/instant/1z10-dobrze-8345/embed/" frameborder="0" scrolling="no"></iframe>
-      <p>DOBRZE</p>
-    </div>
-    <div class="inputholder">
-      <iframe width="110" height="110" src="https://www.myinstants.com/instant/1z10-zle-81313/embed/" frameborder="0" scrolling="no"></iframe>
-      <p>ŹLE</p>
-    </div>
-    <div class="inputholder">
-      <iframe width="110" height="110" src="https://www.myinstants.com/instant/1z10-intro-23781/embed/" frameborder="0" scrolling="no"></iframe>
-      <p>PRZERWA</p>
-    </div>
-    <div class="inputholder"></div>
-    <div class="inputholder"></div>
-    <div class="inputholder"></div>
-    <div class="inputholder"></div>
-    <div class="inputholder"></div>
-
-
-  </div>
-
-
-</template>
 
 <style scoped>
 
-.inputholder {
+.inputholder, 
+.buttonholder, 
+.pointsholder {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.buttonholder {
+  flex-direction: row;
+  margin: 1rem;
 }
 .playerholder {
   width: 100%;
@@ -142,6 +186,16 @@
 .chance {
   display: flex;
   width: 25%;
+  height: 100%;
+}
+
+.pointsholder {
+  flex-direction: row;
+  height: 75%;
+}
+
+.pointsdiv {
+  width: 33%;
   height: 100%;
 }
 
